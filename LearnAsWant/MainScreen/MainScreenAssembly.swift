59ecf75a17afd645892e22674a4b9b92ac43de39
Aleksei <a.mikhlev@gmail.com>
2023-02-02 19:13:03 +0800

    Add program openning

--- LearnAsWant/MainScreen/MainScreenAssembly.swift
+++ LearnAsWant/MainScreen/MainScreenAssembly.swift
@@ -9,20 +9,11 @@
 import UIKit
 
  class MainScreenAssembly {
-    let navigationController: UINavigationController
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
 
     func create() -> MainScreenViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        guard let viewController = storyboard.instantiateViewController(
-            identifier: String(describing: MainScreenViewController.self)
-        ) as? MainScreenViewController else {
-            preconditionFailure("Cannot create MainScreenViewController")
-        }
+        let viewController = MainScreenViewController()
 
+        let navigationController = UINavigationController(rootViewController: viewController)
         let router = MainScreenRouter(navigationController: navigationController)
         let presenter = MainScreenPresenter(view: viewController, router: router)
         viewController.presenter = presenter
