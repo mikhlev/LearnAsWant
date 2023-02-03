a78850631583f149a7efcfc8df314e82b0a01663
Aleksei <a.mikhlev@gmail.com>
2023-02-03 16:17:00 +0800

    Add translation model

---
+++ LearnAsWant/Models/TranlsationModel.swift
@@ -0,0 +1,37 @@
+//
+//  TranlsationModel.swift
+//  LearnAsWant
+//
+//  Created by Aleksey on 03.02.2023.
+//
+
+import Foundation
+import MLKitTranslate
+
+class TranlsationModel {
+    var fromLanguage: GlobalLanguage
+    var toLanguage: GlobalLanguage
+    var fromText: String?
+    var toText: String?
+
+    init(fromLanguage: GlobalLanguage, toLanguage: GlobalLanguage, fromText: String? = nil, toText: String? = nil) {
+        self.fromLanguage = fromLanguage
+        self.toLanguage = toLanguage
+        self.fromText = fromText
+        self.toText = toText
+    }
+}
+
+enum GlobalLanguage: String {
+    case russian = "Russian"
+    case english = "English"
+
+    var libraryName: TranslateLanguage {
+        switch self {
+        case .russian:
+            return .russian
+        case .english:
+            return .english
+        }
+    }
+}
