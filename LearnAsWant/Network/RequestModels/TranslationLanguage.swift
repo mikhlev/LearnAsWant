3ab133cc57dd64a9d9952b2115178eb477a8733c
Aleksei <a.mikhlev@gmail.com>
2023-02-17 12:05:10 +0800

    Add request model

---
+++ LearnAsWant/Network/RequestModels/TranslationLanguage.swift
@@ -0,0 +1,13 @@
+//
+//  TranslationLanguage.swift
+//  LearnAsWant
+//
+//  Created by Aleksey on 17.02.2023.
+//
+
+import Foundation
+
+struct TranslationLanguage {
+    var code: String?
+    var name: String?
+}
