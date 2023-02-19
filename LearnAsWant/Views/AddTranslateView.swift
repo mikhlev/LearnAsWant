c0043509fe9888f383da471da0b47900949fbf01
Aleksei <a.mikhlev@gmail.com>
2023-02-19 14:05:40 +0800

    Add translate view

---
+++ LearnAsWant/Views/AddTranslateView.swift
@@ -0,0 +1,20 @@
+//
+//  AddTranslateView.swift
+//  LearnAsWant
+//
+//  Created by Aleksey on 19.02.2023.
+//
+
+import UIKit
+
+class AddTranslateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
