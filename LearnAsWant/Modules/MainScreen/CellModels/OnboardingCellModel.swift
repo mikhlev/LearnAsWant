4cb4b521cbfaf07b218307b6941d0f523afec938
Aleksei <a.mikhlev@gmail.com>
2023-03-08 14:12:14 +0800

    Update onboarding cell

---
+++ LearnAsWant/Modules/MainScreen/CellModels/OnboardingCellModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingCellModel.swift
+//  LearnAsWant
+//
+//  Created by Aleksey on 08.03.2023.
+//
+
+import Foundation
+
+struct OnboardingCellModel: PTableViewCellModel {
+
+    func configure(cell: OnboardingCell) {
+        
+    }
+}
--- LearnAsWant/Modules/MainScreen/Cells/OnboardingCell.swift
+++ LearnAsWant/Modules/MainScreen/Cells/OnboardingCell.swift
@@ -9,15 +9,49 @@ import UIKit
 
 class OnboardingCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private lazy var line: DashedView = {
+        let view = DashedView()
+        view.backgroundColor = .clear
+        return view
+    }()
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        label.text = "Add first card"
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        setupViews()
+        setupConstraints()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init?(coder: NSCoder) has not been implemented")
     }
 
+    private func setupViews() {
+        self.contentView.addSubviews(line, label)
+    }
+
+    private func setupConstraints() {
+        line.snp.makeConstraints { make in
+            make.width.equalTo(1)
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalTo(200)
+        }
+
+        label.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.top.equalTo(line.snp.bottom).offset(6)
+            make.left.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
 }
