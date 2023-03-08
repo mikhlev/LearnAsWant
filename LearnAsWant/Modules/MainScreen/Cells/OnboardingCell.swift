39b0252d48ad1a86edbf1a7ce2329fbe948cfae8
Aleksei <a.mikhlev@gmail.com>
2023-03-08 13:51:43 +0800

    Add onboarding cell

---
+++ LearnAsWant/Modules/MainScreen/Cells/OnboardingCell.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingCell.swift
+//  LearnAsWant
+//
+//  Created by Aleksey on 08.03.2023.
+//
+
+import UIKit
+
+class OnboardingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
